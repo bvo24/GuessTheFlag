f3ccdb1d0770b0267ea1fdfaba9d0934d5bddb6f
bvo24 <brianvo2020@gmail.com>
2024-05-18 16:41:27 -0400

    -Added accessibility label, so you can hear a description of the flag not the flag name

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -50,6 +50,22 @@ struct ContentView: View {
     
     @State private var flagSelected : Int? = nil
     
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes. Top stripe blue, middle stripe black, bottom stripe white.",
+        "France": "Flag with three vertical stripes. Left stripe blue, middle stripe white, right stripe red.",
+        "Germany": "Flag with three horizontal stripes. Top stripe black, middle stripe red, bottom stripe gold.",
+        "Ireland": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe orange.",
+        "Italy": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe red.",
+        "Nigeria": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe green.",
+        "Poland": "Flag with two horizontal stripes. Top stripe white, bottom stripe red.",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red.",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background.",
+        "Ukraine": "Flag with two horizontal stripes. Top stripe blue, bottom stripe yellow.",
+        "US": "Flag with many red and white stripes, with white stars on a blue background in the top-left corner."
+    ]
+    
+    
+    
     var body: some View {
         
         ZStack{
@@ -92,6 +108,7 @@ struct ContentView: View {
                             flagImage(image: countries[number])
                                 
                         }
+                        .accessibilityLabel(labels[countries[number]], default: "Unknown flag")
                         .rotation3DEffect(flagSelected == number ? .degrees(360.0): .zero ,axis: (x:0 , y: 1, z: 0))
                         .opacity(flagSelected != nil && flagSelected != number ? 0.25 : 1)
                         .scaleEffect(flagSelected != nil && flagSelected != number ? 0.75 : 1)
